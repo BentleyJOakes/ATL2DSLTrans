4a61b2e469d6f78bf44cfd21b8fd227ad19c63dc
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-09-26 13:09:28 -0400

    Uncomment downloading and add stub for last step.

---
+++ converter/ConvertATL2DSLTrans.py
@@ -0,0 +1,7 @@
+class ConvertATL2DSLTrans:
+
+    def __init__(self, trans_dir):
+        self.trans_dir = trans_dir
+
+    def run(self):
+        print("Not implemented yet.")
--- converter/zoo_to_dsltrans.py
+++ converter/zoo_to_dsltrans.py
@@ -4,7 +4,7 @@ from ZipDownloader import ZipDownloader
 from ZipHandler import ZipHandler
 from ConvertATL2XMI import ConvertATL2XMI
 from RTEHandler import RTEHandler
-
+from ConvertATL2DSLTrans import ConvertATL2DSLTrans
 
 class ZooConverter:
 
@@ -29,6 +29,10 @@ class ZooConverter:
         rte = RTEHandler(self.trans_dir)
         rte.run()
 
+    def run_atl_to_dsltrans(self):
+        catd = ConvertATL2DSLTrans(self.trans_dir)
+        catd.run()
+
 if __name__ == "__main__":
 
     zoo_site = "https://www.eclipse.org/atl/atlTransformations/"
@@ -36,10 +40,12 @@ if __name__ == "__main__":
     trans_dir = "./example_dir"
     zc = ZooConverter(zip_dir, trans_dir)
 
-    #zc.download_zips(zoo_site)
+    zc.download_zips(zoo_site)
 
     zc.separate_transformations()
 
     zc.run_atl_to_xmi()
 
     zc.run_types_trans()
+
+    zc.run_atl_to_dsltrans()
