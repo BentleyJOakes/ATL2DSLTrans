91b6f2ed2753d587aa5e2af1f1a24f2297155fac
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-10-10 16:36:33 -0400

    Record successful rule type extractions. Also try all combos of metamodels, and save output to directory. Could be made more intelligent.

--- converter/RTEHandler.py
+++ converter/RTEHandler.py
@@ -11,28 +11,48 @@ class RTEHandler:
     def run(self):
         dirs = sorted([os.path.join(self.trans_dir, d) for d in os.listdir(self.trans_dir) if
                        os.path.isdir(os.path.join(self.trans_dir, d))])
+
+        num_success = 0
+        print("Extracting rule types for " + str(len(dirs)) + " transformations")
         for d in dirs:
             files = sorted([f for f in os.listdir(d)])
             print(files)
 
-            source_MM = [x for x in files if x.endswith(".ecore") and x.startswith("source_")][0]
-            target_MM = [x for x in files if x.endswith(".ecore") and x.startswith("target_")][0]
+            mms = [x for x in files if x.endswith(".ecore")]
             trans = [x for x in files if x.endswith(".atl")][0]
 
-            source_MM_path = os.path.abspath(os.path.join(d, source_MM))
-            target_MM_path = os.path.abspath(os.path.join(d, target_MM))
             trans_path = os.path.abspath(os.path.join(d, trans))
             path = os.path.abspath(d)
 
-            # print(source_MM)
+            #print(source_MM)
             # print(target_MM)
             # print(trans)
 
             current_path = os.getcwd()
             os.chdir(self.jar_loc)
 
-            command = "java -jar " + self.jar_name + " " + source_MM_path  + " " + target_MM_path  + " " + trans_path + " " + path
-            print("Running: " + command)
-            subprocess.run(command, shell=True)
+            #try all combos of mms
+            for mm1 in mms:
+                mm1_name = mm1.replace("mm_", "").replace(".ecore", "")
+                mm1_path = os.path.abspath(os.path.join(path, mm1))
+                #print(mm1_path)
+                for mm2 in mms:
+                    mm2_name = mm2.replace("mm_", "").replace(".ecore", "")
+                    mm2_path = os.path.abspath(os.path.join(path, mm2))
+
+                    suffix = "-" + mm1_name + '-' + mm2_name
+                    command = "java -jar " + self.jar_name + " " + mm1_path  + " " + mm2_path  + " " + trans_path + " " + path + " " + suffix
+                    print("Running: " + command)
+                    out_filename = os.path.join(path, "out" + suffix + ".txt")
+                    with open(out_filename, 'w') as o:
+                        subprocess.run(command, shell=True, stderr = subprocess.STDOUT, stdout=o)
 
             os.chdir(current_path)
+
+            files = sorted([f for f in os.listdir(d)])
+            types = [f for f in files if "Types" in f]
+            if types:
+                num_success += 1
+
+
+        print("Successfully found types for " + str(num_success) + " transformations.")
