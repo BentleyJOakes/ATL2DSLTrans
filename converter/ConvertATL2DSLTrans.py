e4252e5f313c8fd591abf2d201f6e6de04809071
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-10-10 23:25:40 -0400

    Move files and create master script.

--- converter/ConvertATL2DSLTrans.py
+++ converter/ConvertATL2DSLTrans.py
@@ -1,74 +1,152 @@
 import os
+import shutil
+
 class ConvertATL2DSLTrans:
 
     def __init__(self, trans_dir):
         self.trans_dir = trans_dir
 
         self.dest_dir = "../ATL2DSLTrans/"
-        self.dest_dir_examples = os.path.join(self.dest_dir, "examples")
+        self.examples_dir = "examples"
+        self.dest_dir_examples = os.path.join(self.dest_dir, self.examples_dir)
         self.dest_dir_configs = os.path.join(self.dest_dir, "configs")
         self.base_script = os.path.join(self.dest_dir, "ATL2DSLTrans_BaseScript.launch")
 
         self.launch_group_file = os.path.join(self.dest_dir, "ATL2DSLTrans.launch")
 
+
+        self.trans_moved = []
+        self.trans_moved_dirs = {}
+
     def set_up(self):
 
         print("Setting up conversion from ATL to DSLTrans")
 
+        no_trans_xmi = 0
+        no_types_xmi = 0
+
         #move needed files
         dirs = sorted([os.path.join(self.trans_dir, d) for d in os.listdir(self.trans_dir) if os.path.isdir(os.path.join(self.trans_dir, d))])
         for d in dirs:
-            trans_name = str(d.split("/")[-1])
+            trans_name = str(d.split("/")[-1]).replace(".atl", "")
             #print(trans_name + b".xmi")
             trans_xmi = [f for f in os.listdir(d) if f == trans_name + ".xmi"]
             if not trans_xmi:
                 #print("Error: Transformation XMI not found in " + d)
+                no_trans_xmi += 1
                 continue
+            else:
+                trans_xmi = trans_xmi[0]
 
             types_xmi = [f for f in os.listdir(d) if f.endswith("Types.xmi")]
 
 
             if not types_xmi:
                 #print("Error: Types XMI not found in " + d)
+                no_types_xmi += 1
                 continue
+            else:
+                types_xmi = types_xmi[0]
 
-            print(trans_xmi)
-            print(types_xmi)
+            mms = [f for f in os.listdir(d) if f.endswith(".ecore")]
 
-            # files = sorted([f for f in os.listdir(d) if f.endswith(".atl")])
-            # for f in files:
-            #     trans_name = f.replace(".atl", "")
-            #     full_filename = os.path.join(d, f)
-            #     shutil.copyfile(full_filename, os.path.join(self.dest_dir_examples, f))
-            #
-            #     #record this transformation moved over
-            #     if trans_name not in self.trans_moved:
-            #         self.trans_moved.append(trans_name)
-            #
-            #     self.trans_moved_dirs[trans_name] = d
-            #
-            #     with open(self.base_script) as base:
-            #
-            #         new_name = "convert_" + trans_name + ".launch"
-            #         new_script = os.path.join(self.dest_dir_configs, new_name)
-            #         with open(new_script, 'w') as new:
-            #             for line in base:
-            #
-            #                 new_line = line.replace("Composed2Simple", trans_name)
-            #                 new.write(new_line + "\n")
+            source_mm = None
+            source_mm_name = None
 
-        # with open(self.launch_group_file, 'w') as lg:
-        #     lg.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
-        #     lg.write('<launchConfiguration type="org.eclipse.debug.core.groups.GroupLaunchConfigurationType">\n')
-        #     for i, trans in enumerate(self.trans_moved):
-        #         lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.action" value="WAIT_FOR_TERMINATION"/>\n'.format(i))
-        #         lg.write('<booleanAttribute key="org.eclipse.debug.core.launchGroup.{0}.adoptIfRunning" value="false"/>\n'.format(i))
-        #         lg.write('<booleanAttribute key="org.eclipse.debug.core.launchGroup.{0}.enabled" value="true"/>\n'.format(i))
-        #         lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.mode" value="inherit"/>\n'.format(i))
-        #         lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.name" value="convert_{1}"/>\n'.format(i, trans))
-        #     lg.write('</launchConfiguration>\n')
-        #
-        # print("Moved " + str(len(self.trans_moved)) + " ATL files")
+            target_mm = None
+            target_mm_name = None
+
+            for mm in mms:
+                mm_name = mm.replace("mm_", "").replace(".ecore", "")
+
+                if trans_name.lower().startswith(mm_name.lower()):
+                    source_mm = mm
+                    source_mm_name = mm_name
+                elif trans_name.lower().endswith(mm_name.lower()):
+                    target_mm = mm
+
+            if len(mms) == 1:
+                source_mm = mms[0]
+                target_mm = mms[0]
+
+            # try to autodetect source and target mms
+            if source_mm and not target_mm:
+                if source_mm_name in mms[0]:
+                    target_mm = mms[1]
+                else:
+                    if source_mm_name in mms[1]:
+                        target_mm = mms[0]
+
+            if not (source_mm and target_mm):
+                s = "Error: Couldn't find source and target metamodels from transformation name!\n"
+                s += "Metamodels: " + str(mms) + "\n"
+                s += str(source_mm)
+                s += str(target_mm)
+                s += "Transformation name: " + trans_name
+                raise Exception(s)
+
+            # print("Trans XMI: " + trans_xmi)
+            # print(types_xmi)
+            # print(source_mm)
+            # print(target_mm)
+            # print(trans_name)
+
+
+
+            trans_dir = os.path.join(self.dest_dir_examples, trans_name)
+            if not os.path.exists(trans_dir):
+                os.mkdir(trans_dir)
+
+
+            files_to_move = [trans_xmi, types_xmi, source_mm, target_mm]
+
+            files = sorted([f for f in os.listdir(d)])
+            for f in files:
+                if f in files_to_move:
+                    full_filename = os.path.join(d, f)
+                    shutil.copyfile(full_filename, os.path.join(trans_dir, f))
+                    #print(full_filename + " - " + os.path.join(trans_dir, f))
+
+                #record this transformation moved over
+                if trans_name not in self.trans_moved:
+                    self.trans_moved.append(trans_name)
+
+                self.trans_moved_dirs[trans_name] = d
+
+                new_name = "convert_" + trans_name + ".launch"
+                new_script = os.path.join(self.dest_dir_configs, new_name)
+
+                replace_keys = {
+                    "*XMIFILE*":os.path.join(self.examples_dir, trans_name, trans_xmi),
+                    "*INMM*": os.path.join(self.examples_dir, trans_name, source_mm),
+                    "*OUTMM*": os.path.join(self.examples_dir, trans_name, target_mm),
+                    "*TYPESFILE*": os.path.join(self.examples_dir, trans_name, types_xmi),
+                    "*OUTFILENAME*": trans_name,
+                }
+                with open(self.base_script) as base:
+                    with open(new_script, 'w') as new:
+                        for line in base:
+                            for k, v in replace_keys.items():
+                                line = line.replace(k, v)
+                            new.write(line)
+
+        with open(self.launch_group_file, 'w') as lg:
+            lg.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
+            lg.write('<launchConfiguration type="org.eclipse.debug.core.groups.GroupLaunchConfigurationType">\n')
+            for i, trans in enumerate(self.trans_moved):
+                lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.action" value="DELAY"/>\n'.format(i))
+                lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.actionParam" value="3"/>\n'.format(i))
+                lg.write('<booleanAttribute key="org.eclipse.debug.core.launchGroup.{0}.adoptIfRunning" value="false"/>\n'.format(i))
+                lg.write('<booleanAttribute key="org.eclipse.debug.core.launchGroup.{0}.enabled" value="true"/>\n'.format(i))
+                lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.mode" value="inherit"/>\n'.format(i))
+                lg.write('<stringAttribute key="org.eclipse.debug.core.launchGroup.{0}.name" value="convert_{1}"/>\n'.format(i, trans))
+            lg.write('</launchConfiguration>\n')
+
+
+        print("Number of transformations possible: " + str(len(dirs)))
+        print("Failed (no transformation xmi): " + str(no_trans_xmi))
+        print("Failed (no types xmi): " + str(no_types_xmi))
+        print("Successes: " + str(len(dirs) - no_trans_xmi - no_types_xmi))
 
     def convert(self):
 
